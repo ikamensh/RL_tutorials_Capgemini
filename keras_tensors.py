fba7c6d92fdcb274d80c33b7177a9f3f67a96eff
Ilya Kamenshchikov <ikamenshchikov@gmail.com>
2017-10-27 16:08:08 +0200

    Update keras_tensors.py

--- keras_tensors.py
+++ keras_tensors.py
@@ -1,44 +1,29 @@
-from keras.layers import Dense, Dropout, ELU, Input
+from keras.layers import Dense, ELU, Input
 from keras.models import Model
-from keras.optimizers import adam
-from keras.loss import binary_crossentropy
+import tensorflow as tf
 
-ad = adam(lr=1e-3)
+input_size=5
 
 # This returns a tensor
 inputs = Input(shape=(input_size,))
 
-hidden1 = Dense(4 * model_size) (inputs)
+hidden1 = Dense(128) (inputs)
 hidden1a = ELU() (hidden1)
 
-hidden2 = Dense(4 * model_size)(hidden1a)
-hidden2a = ELU()(hidden2)
+some_quantity = tf.Variable(tf.random_normal([128,128]), name = 'some_quantity')
+average_of_quantity = tf.reduce_mean(some_quantity)
+hidden_plus_avg = hidden1a+average_of_quantity
 
-hidden3 = Dense(4 * model_size)(hidden2a)
-hidden3a = ELU()(hidden3)
-hidden3d = Dropout(0.3)(hidden3a)
+hidden2 = Dense(128) (hidden1a)
+hidden2a = ELU() (hidden2)
 
-hidden4 = Dense(2 * model_size)(hidden3d)
-hidden4a = ELU()(hidden4)
-hidden4d = Dropout(0.3)(hidden4a)
-print(hidden4d)
+output = Dense(1, activation='sigmoid') (hidden2a)
 
-hidden5 = Dense(model_size)(hidden4d)
-last_layer = ELU()(hidden5)
-hidden = Dropout(0.3)(last_layer)
-
-output = Dense(1, activation='sigmoid') (hidden)
-
-# This creates a model that includes
-# the Input layer and three Dense layers
+# !!! We define model as everything connecting two Tensors !!!
 model = Model(inputs=inputs, outputs=output)
 
-#Replace output = LAYER_X to your layer to access model that shares weights with the original, but outputs values of desired layer!
-features_model = Model(inputs=inputs, outputs=last_layer)
 
-model.compile(optimizer=ad,
-			  loss=binary_crossentropy)
-			  
-features_model.compile(optimizer=ad,
-			  loss=binary_crossentropy)
-
+print(inputs)
+print(output)
+print(hidden_plus_avg)
+model.summary()
