794387e3de064d50b74dd35d455ce2dfd68a72b3
Ilya Kamenshchikov <ikamenshchikov@gmail.com>
2017-10-27 09:04:58 +0200

    Update mini_tb.py

--- mini_tb.py
+++ mini_tb.py
@@ -11,8 +11,6 @@ from keras.utils import to_categorical
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train.reshape(60000, 784)
-x_train = x_train.astype('float32')
-x_train /= 255
 y_train = to_categorical(y_train, 10)
 
 model = Sequential()
